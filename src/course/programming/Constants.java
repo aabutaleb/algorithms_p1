e19992fd5b5338a3d5606d93e76b34891df56603
Amin <heavywizard@gmail.com>
2013-03-16 13:26:45 +0100

    subo ex6

--- src/course/programming/Constants.java
+++ src/course/programming/Constants.java
@@ -1,5 +1,5 @@
 package course.programming;
 
 public class Constants {
-	public static String RESOURCES_DIR = "C:\\Users\\Amin\\workspace\\AlgorithmsAnalysisDesign_Standford\\src\\course\\programming\\resources";
+	public static String RESOURCES_DIR = "C:\\Users\\Amin\\algorithmsanalysisdesign_course\\src\\course\\programming\\resources";
 }
---
+++ src/course/programming/exercices/ex6/Alg2Sum.java
@@ -0,0 +1,66 @@
+package course.programming.exercices.ex6;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import course.programming.Constants;
+
+public class Alg2Sum {
+	private static final Map<String, Integer> m = new HashMap<String, Integer>(); 
+	private static final String FILE_PATH = Constants.RESOURCES_DIR	+ "\\ex6\\HashInt.txt";
+
+	public static void main(final String[] args) {
+		loadFile();
+
+		int count = 0;
+		for (int t = 2500; t <= 4000; t++){
+			System.out.println("Buscando t = "+t);
+			Set<Entry<String, Integer>> set = new HashSet<Map.Entry<String,Integer>>(m.entrySet());
+			int y = -1;
+			for (int x = 0; x < set.size() && y < 0; x++){
+				Integer aux;
+				if ((aux = m.get(String.valueOf(t-x))) != null && aux.intValue() != x){
+					y = m.get(String.valueOf(t-x));
+					System.out.println("Encontrados x = "+x+ " y = "+y);
+				}
+			}
+
+			if (y >= 0){
+				count++;
+			}
+		}
+
+		System.out.println("TERMINADO: "+count);
+	}
+
+	private static void loadFile() {
+		try {
+			File f = new File(FILE_PATH);
+			if (!f.exists()) {
+				throw new FileNotFoundException();
+			}
+			FileInputStream fstream = new FileInputStream(f);
+
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			// Read File Line By Line
+			while ((strLine = br.readLine()) != null) {
+				m.put(strLine, Integer.parseInt(strLine));
+			}
+			// Close the input stream
+			in.close();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+}
